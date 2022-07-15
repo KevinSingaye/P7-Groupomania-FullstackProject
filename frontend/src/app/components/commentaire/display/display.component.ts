18611cc6ab8222cddc04144a4db2e9bc74da97a8
KevinSingaye <75949789+KevinSingaye@users.noreply.github.com>
2022-07-16 01:02:11 +0200

    ajout supprimer commentaire

--- frontend/src/app/components/commentaire/display/display.component.ts
+++ frontend/src/app/components/commentaire/display/display.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CommentaireService } from './../../../services/commentaire.service';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-display-commentaire',
@@ -6,11 +7,41 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+@Output() output = new EventEmitter();
+@Input() _id:string | undefined = undefined;
 @Input() commentaire:any;
+commentaires: any []=[]
+  currentCommentaires: any;
 
-  constructor() { }
+  constructor(private commentaireService:CommentaireService) { }
 
   ngOnInit(): void {
+  }
+
+
+onDelete():void {
+      this.commentaireService.delete(this.commentaire._id).subscribe((result:any) => {
+      console.log(result)
+      this.output.emit({action: 'DELETE', data: this.commentaire});
+    }, (error: any) =>
+      console.error(error))
+  
+  }
+
+onUpdateOrDelete(data:any):void{
+     let action = data.action;
+  let commentaire= data.data;
+  if(action=== 'DELETE'){
+let index = this.commentaires.findIndex((commentaire)=> commentaire._id === commentaire._id);
+ if (index>-1){
+  this.commentaires.splice(index, 1)
+ }
+  }
+    else{
+      this.currentCommentaires = commentaire;
+    }
+  
+   
   }
 
 }
